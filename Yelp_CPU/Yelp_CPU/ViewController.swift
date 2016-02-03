9cf6b47f7f3bef159d94a9a93df459c2979afa81
chrissoria <csoria1@mail.ccsf.edu>
2016-02-02 20:07:40 -0800

    Yelp app startup

---
+++ Yelp_CPU/Yelp_CPU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yelp_CPU
+//
+//  Created by christopher soria on 2/2/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
