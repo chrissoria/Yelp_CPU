e3d7259221be335eec0a7e8b61b6f02f8ad2189b
chrissoria <csoria1@mail.ccsf.edu>
2016-02-06 17:33:00 -0800

    Issue one
    appDelegate returning issue. Won’t load nameLabel. Blank cell.

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,38 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by christopher soria on 2/6/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
